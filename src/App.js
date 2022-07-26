c155160a221f2c1305e84665d7f657c3218af50d
joy <mahadi.hasan@mobioapp.com>
2022-07-26 10:39:58 +0600

    MULTI LAYOUT IMPLEMENT

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ButtonData from "./ButtonData";
 import Button from "./components/Button";
 import Board from "./components/Board";
 import {useState} from "react";
+import {Routes, Route} from "react-router-dom";
+
 function App() {
     const {winningPatterns, winningPatternsNext} = ButtonData
     const [squares, setSquares] = useState(Array(9).fill(null))
@@ -40,20 +42,26 @@ function App() {
         }
     }
     return (<div className="App">
-        {restart?<GameLayout winningPatterns = {winningPatterns} winningPatternsNext={winningPatternsNext} >
-            <Board winningPatterns={winningPatterns} winningPatternsNext={winningPatternsNext} class = "board">
-                <Button className1="button" className2="button2" value={squares[0]} onClick={() => handleClick(0)}/>
-                <Button className1="button" className2="button2" value={squares[1]} onClick={() => handleClick(1)}/>
-                <Button className1="button" className2="button2" value={squares[2]} onClick={() => handleClick(2)}/>
-                <Button className1="button" className2="button2" value={squares[3]} onClick={() => handleClick(3)}/>
-                <Button className1="button" className2="button2" value={squares[4]} onClick={() => handleClick(4)}/>
-                <Button className1="button" className2="button2" value={squares[5]} onClick={() => handleClick(5)}/>
-                <Button className1="button" className2="button2" value={squares[6]} onClick={() => handleClick(6)}/>
-                <Button className1="button" className2="button2" value={squares[7]} onClick={() => handleClick(7)}/>
-                <Button className1="button" className2="button2" value={squares[8]} onClick={() => handleClick(8)}/>
-            </Board>
-            <Button classname1="button" value = "EXIT" onClick={()=>window.location.reload()}></Button>
-        </GameLayout>:<GameLayout><Button classname1="button" value = "START" onClick={()=>setReStart(!start)}></Button></GameLayout>}
+        <Routes>
+            <Route path={"/"} element={<GameLayout winningPatterns = {winningPatterns} winningPatternsNext={winningPatternsNext} ></GameLayout>}>
+                <Route path ={"start"} element={ <Board winningPatterns={winningPatterns} winningPatternsNext={winningPatternsNext} class = "board">
+                    <Button className1="button" className2="button2" value={squares[0]} onClick={() => handleClick(0)}/>
+                    <Button className1="button" className2="button2" value={squares[1]} onClick={() => handleClick(1)}/>
+                    <Button className1="button" className2="button2" value={squares[2]} onClick={() => handleClick(2)}/>
+                    <Button className1="button" className2="button2" value={squares[3]} onClick={() => handleClick(3)}/>
+                    <Button className1="button" className2="button2" value={squares[4]} onClick={() => handleClick(4)}/>
+                    <Button className1="button" className2="button2" value={squares[5]} onClick={() => handleClick(5)}/>
+                    <Button className1="button" className2="button2" value={squares[6]} onClick={() => handleClick(6)}/>
+                    <Button className1="button" className2="button2" value={squares[7]} onClick={() => handleClick(7)}/>
+                    <Button className1="button" className2="button2" value={squares[8]} onClick={() => handleClick(8)}/>
+                </Board>}>
+
+                </Route>
+                <Route index element={<Button classname1="button" value = "Welcome To The Game" onClick={()=>setReStart(!start)}></Button>}>
+
+                </Route>
+            </Route>
+        </Routes>
     </div>);
 }
 
--- src/components/Layouts/GameLayout.jsx
+++ src/components/Layouts/GameLayout.jsx
@@ -1,9 +1,11 @@
+import {Outlet} from "react-router-dom";
+
 export default function GameLayout(props) {
 
     return (
         <div>
             <h1 className="center"> TIC-TAC-TOE</h1>
-            {props.children}
+           <Outlet/>
             <p className="center">--better learn--</p>
         </div>
     )
